cde6ee9d261b75dfca2b28c777df510d22f848e4
Antonio Skorin <antonio.skorin@gmail.com>
2026-06-04 19:21:57 -0400

    Feat: casos por estudiante

--- backend/app/api/students.py
+++ backend/app/api/students.py
@@ -1,7 +1,8 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from app.crud.cases import get_cases_for_user
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.crud.curricular import get_students, get_courses
+from app.crud.curricular import get_student_by_id, get_students, get_courses
 from app.database.database import get_db
 from app.api.deps import RoleChecker
 
@@ -27,3 +28,17 @@ async def get_all_courses(
     db: AsyncSession = Depends(get_db)
 ):
     return await get_courses(db, user)
+
+
+@router.get("/students/{id_estudiante}/cases")
+async def get_cases_for_student(
+    id_estudiante: str,
+    user: Annotated[
+        dict, Depends(RoleChecker(allowed_roles=["coordinador", "profesor_jefe"]))
+    ],
+    db: AsyncSession = Depends(get_db)
+):
+    estudiante = await get_student_by_id(db, user, id_estudiante)
+    if not estudiante:
+        raise HTTPException(status_code=404, detail="Estudiante no encontrado")
+    return await get_cases_for_user(db, user, id_estudiante)
